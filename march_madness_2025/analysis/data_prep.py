05f614a348c7147c739b0db392c11d180d711da4
Jake Campbell <jake@jakes-air.mynetworksettings.com>
2026-01-23 22:42:56 -0500

    Data prep script update to read raw files

--- march_madness_2025/analysis/data_prep.py
+++ march_madness_2025/analysis/data_prep.py
@@ -6,6 +6,12 @@ import os
 # Setting working directory
 os.chdir("/Users/jake/Documents/project_repo/march_madness_2025")
 
-# Loading in data files
-data_file_names = [i for i in os.listdir("data") if not i.startswith(".")]
 
+# Loading in data files
+data_file_names = sorted([i for i in os.listdir("data") if not i.startswith(".")])
+
+# Going to create a dictionary of raw data frames
+raw_data = {}
+for i in data_file_names:
+    df = pd.read_csv("data/" + i)
+    raw_data[i.split(".")[0]] = df
